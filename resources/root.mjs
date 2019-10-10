156ff8a85ff389d1572aae4332f5045c902e5641
metastable-void <git@yuka.h09.eu>
2019-10-11 02:22:10 +0900

    update

--- resources/root.mjs
+++ resources/root.mjs
@@ -80,3 +80,7 @@ document.addEventListener ('load', e => {
 	console.log (decodeURIComponent ('%0a%5f%5f%5f%5f%5f%5f%5f%5f%5f%5f%5f%5f%5f%5f%5f%5f%5f%5f%5f%5f%5f%5f%5f%5f%5f%5f%5f%5f%5f%5f%5f%5f%5f%5f%5f%5f%5f%5f%5f%5f%5f%5f%5f%5f%5f%5f%5f%5f%5f%5f%5f%5f%5f%5f%5f%5f%5f%5f%5f%5f%5f%5f%5f%5f%5f%5f%5f%5f%5f%5f%5f%5f%5f%5f%5f%5f%5f%5f%5f%5f%0a%0a%e6%9d%b1%e5%a4%a7%e5%a5%b3%e8%a3%85%e5%ad%90%e3%82%b3%e3%83%b3%e3%83%86%e3%82%b9%e3%83%88%e5%ae%9f%e8%a1%8c%e5%a7%94%e5%93%a1%e4%bc%9a%32%30%31%37%2d%32%30%31%38%0a%57%65%62%e3%83%87%e3%82%b6%e3%82%a4%e3%83%b3%e3%82%a2%e3%82%b7%e3%82%b9%e3%82%bf%e3%83%b3%e3%83%88%e5%8b%9f%e9%9b%86%ef%bc%81%0a%e5%88%9d%e5%bf%83%e8%80%85%e5%8f%af%e3%83%bb%e3%83%97%e3%83%ad%e3%82%b0%e3%83%a9%e3%83%9f%e3%83%b3%e3%82%b0%e7%9f%a5%e8%ad%98%e4%b8%8d%e5%95%8f%e3%83%bb%e3%82%b0%e3%83%a9%e3%83%95%e3%82%a3%e3%83%83%e3%82%af%e3%83%87%e3%82%b6%e3%82%a4%e3%83%b3%e3%82%84%e3%82%bf%e3%82%a4%e3%83%9d%e3%82%b0%e3%83%a9%e3%83%95%e3%82%a3%e3%81%ab%e8%88%88%e5%91%b3%e3%81%8c%e3%81%82%e3%82%8b%e4%ba%ba%e6%ad%93%e8%bf%8e%0a%e9%80%a3%e7%b5%a1%e5%85%88%20%54%77%69%74%74%65%72%3a%20%40%5f%75%74%73%32%0a%5f%5f%5f%5f%5f%5f%5f%5f%5f%5f%5f%5f%5f%5f%5f%5f%5f%5f%5f%5f%5f%5f%5f%5f%5f%5f%5f%5f%5f%5f%5f%5f%5f%5f%5f%5f%5f%5f%5f%5f%5f%5f%5f%5f%5f%5f%5f%5f%5f%5f%5f%5f%5f%5f%5f%5f%5f%5f%5f%5f%5f%5f%5f%5f%5f%5f%5f%5f%5f%5f%5f%5f%5f%5f%5f%5f%5f%5f%5f%5f%0a'));
 });
 
+window.addEventListener ('DOMContentLoaded', e => {
+	document.documentElement.classList.remove ('removed');
+});
+
