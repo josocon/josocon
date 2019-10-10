14e993c1c481ac51f90dc58f1d857e7a40e21eea
metastable-void <git@yuka.h09.eu>
2019-10-11 00:51:04 +0900

    fix

--- _tests/index.php
+++ _tests/index.php
@@ -39,7 +39,7 @@ ________________________________________________________________________________
 <meta name='viewport' content='width=device-width, initial-scale=1'/>
 <link rel='stylesheet' href='/resources/root.css'/>
 <script defer='' src='/resources/root.mjs'/>
-<script src='https://cdnjs.cloudflare.com/ajax/libs/markdown-it/10.0.0/markdown-it.min.js' integrity='sha256-YASERpEeN8gRNr/Fy4Km34WGFqIq1h6HkJMAQnVHlhk=' crossorigin='anonymous'/>
+<script src='https://cdnjs.cloudflare.com/ajax/libs/markdown-it/10.0.0/markdown-it.min.js' crossorigin='anonymous'/>
 </head>
 <body is='josocon-page'>
 <div slot='page-notice'>サイト準備中</div>
