77a6519dfbabbe6fc027295625a07d52b4f7a0b0
metastable-void <git@yuka.h09.eu>
2019-10-19 23:03:30 +0900

    updates

---
+++ captcha/index.php
@@ -0,0 +1,51 @@
+<?php /* -*- tab-width: 4; indent-tabs-mode: t -*-
+vim: ts=4 noet ai */
+
+namespace josocon;
+
+/**
+	Copyright 2019 (C) 東大女装子コンテスト実行委員会
+
+	Licensed under the Apache License, Version 2.0 (the "License");
+	you may not use this file except in compliance with the License.
+	You may obtain a copy of the License at
+
+	https://www.apache.org/licenses/LICENSE-2.0
+
+	Unless required by applicable law or agreed to in writing, software
+	distributed under the License is distributed on an "AS IS" BASIS,
+	WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+	See the License for the specific language governing permissions and
+	limitations under the License.
+
+	@license Apache-2.0
+	@file
+*/
+
+
+require_once __DIR__ . '/../_includes/template.php';
+
+$text = "12345";
+
+$image = new \Imagick ();
+$draw = new \ImagickDraw ();
+$color = new \ImagickPixel ('#000000');
+$background = new \ImagickPixel ('none'); // Transparent
+
+$draw->setFont ('Arial');
+$draw->setFontSize (50);
+$draw->setFillColor ($color);
+$draw->setStrokeAntialias (true);
+$draw->setTextAntialias (true);
+
+$metrics = $image->queryFontMetrics ($draw, $text);
+
+$draw->annotation (0, $metrics['ascender'], $text);
+
+$image->newImage ($metrics['textWidth'], $metrics['textHeight'], $background);
+$image->setImageFormat ('png');
+$image->drawImage ($draw);
+
+\header ('content-type: text/png');
+echo $image;
+
