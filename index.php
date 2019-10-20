f3ae47a0aef2ba4c9bdd041325431113f4dab2f8
metastable-void <git@yuka.h09.eu>
2019-10-20 17:12:09 +0900

    css

--- index.php
+++ index.php
@@ -108,7 +108,7 @@ if ('' === $path) {
 			$pictures = $db->getItemPictures ($item);
 			foreach ($pictures as $picture) {
 				echo "<li><figure>";
-				\printf ("<div class='widescreen'><img src='%s'/></div>"
+				\printf ("<div class='widescreen'><div><img src='%s'/></div></div>"
 					, escape ($picture->uri));
 				
 				if (Session::isLoggedIn ()) {
